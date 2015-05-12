05223099b2a44cd2bf4f067da97d4f4cdd74de5f
Dan Seminara <semi225599@gmail.com>
2015-05-11 20:54:58 -0400

    change to v3

--- umph.py
+++ umph.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import os
 import requests
 from docopt import docopt
 from queue import Queue
@@ -10,18 +11,20 @@ class fdict(dict):
         return '{%s}' % key
 
 MAX_RESULT_LIMIT = 50
+YOUTUBE_API_KEY = os.environ['YOUTUBE_API_KEY']
 
-playlist_url = 'https://gdata.youtube.com/feeds/api/playlists/{id}?v=2&alt=json&start-index={start_index}&max-results={max_results}&strict=true'
-upload_url   = 'https://gdata.youtube.com/feeds/api/users/{username}/uploads?v=2&alt=json&start-index={start_index}&max-results={max_results}&strict=true'
+playlist_url = 'https://www.googleapis.com/youtube/v3/playlistItems?key={api_key}&part=contentDetails&playlistId={id}&maxResults={max_results}'.format(**fdict(api_key=YOUTUBE_API_KEY))
+upload_url   = 'https://www.googleapis.com/youtube/v3/channels?key={api_key}&part=contentDetails&forUsername={username}&maxResults={max_results}'.format(**fdict(api_key=YOUTUBE_API_KEY))
+video_url    = 'https://www.youtube.com/watch?v={}'
+
 
 __doc__ = """
-Usage: umph [-a|-m <num>] [-t <type>] [-s <num>] <id>
+Usage: umph [-a|-m <num>] [-t <type>] <id>
        umph -h | --help
 
 Options:
  -h, --help                           Print help and exit
  -t, --type=TYPE                      Set the type of feed to retrieve [default: p]
- -s, --start-index=START              Index of first matching result [default: 1]
  -m, --max-results=MAX                Max number of results included [default: 50]
  -a, --all                            Get the entire feed
 
@@ -31,51 +34,37 @@ def download_page(url):
     return requests.get(url).json()
 
 def print_links(page):
-    for entry in page['feed']['entry']:
-        print(entry['link'][0]['href'])
+    for entry in page['items']:
+        print(video_url.format(entry['contentDetails']['videoId']))
 
-class Downloader(Thread):
-    def __init__(self, queue):
-        Thread.__init__(self)
-        self.queue = queue
+def run_playlists(playlist_id, max_results, get_all):
+    url = playlist_url.format(**fdict(id=playlist_id, max_results=max_results))
+    page = download_page(url)
+    print_links(page)
+    next_page = page.get('nextPageToken')
 
-    def run(self):
-        while True:
-            url = self.queue.get()
-            print_links(download_page(url))
-            self.queue.task_done()
+    if get_all:
+        while next_page:
+            page = download_page(url + '&pageToken={}'.format(next_page))
+            print_links(page)
+            next_page = page.get('nextPageToken')
 
+def run_username(username, max_results, get_all):
+    url = upload_url.format(**fdict(username=username, max_results=max_results))
+    page = download_page(url)
+    items = page.get('items')
+    assert len(items) == 1
+    run_playlists(items[0]['contentDetails']['relatedPlaylists']['uploads'], max_results, get_all)
+    
 def main():
     args = docopt(__doc__)
 
-    start_index = int(args['--start-index'])
     m = min(MAX_RESULT_LIMIT, int(args['--max-results']))
 
     if args['--type'] == 'p':
-        feed_url = playlist_url.format(**fdict(id=args['<id>']))
+        run_playlists(args['<id>'], m, args['--all'])
     elif args['--type'] == 'u':
-        feed_url = upload_url.format(**fdict(username=args['<id>']))
-
-    url = feed_url.format(**fdict(start_index=start_index, max_results=m))
-    page = download_page(url)
-    print_links(page)
-    total_results = page['feed']['openSearch$totalResults']['$t']
-    if not args['--all']:
-        total_results = min(total_results, int(args['--max-results']))
-    start_index += m
-
-    if start_index <= total_results:
-        queue = Queue()
-
-        for _ in range(4):
-            worker = Downloader(queue)
-            worker.daemon = True
-            worker.start()
-
-        for i in range(start_index, total_results+1, m):
-            queue.put(feed_url.format(**fdict(start_index=i, max_results=min(total_results-i+1, m))))
-
-        queue.join()
+        run_username(args['<id>'], m, args['--all'])
 
 if __name__ == '__main__':
     main()
